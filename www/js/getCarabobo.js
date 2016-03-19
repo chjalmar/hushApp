2da464ae1f1ac5ff2a1699b7bfad9f73071e39e8
unknown <chjalmar@gmail.com>
2016-03-19 18:46:10 -0430

    Commit Inicial del esqueleto

---
+++ www/js/getCarabobo.js
@@ -0,0 +1,34 @@
+function devuelveLista() { 
+
+  var playlists = [
+      { title: 'Lolo', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 },
+      { title: 'Chirri', id: 7 }
+    ];
+    
+  
+/*var contenidoWeb = '';
+	
+			$.ajax({ type: 'GET',   
+				 url: 'http://localhost:8100/hush',
+				 async: false,
+				 success : function(text)
+				 {
+				     contenidoWeb = text;
+				 }
+			});
+*/  
+  /* en la variable contenidoWeb tengo todo el contenido HTML de la página Web. Parsearlo para:
+  - crear a partir de ahí una versión en texto del contenido, para mostrar en el actual "playlists" que devuelvo al final de esta función
+  - obtener las categorías con las que quisiera crear el menú de la izquierda */
+  
+  
+/*console.log(contenidoWeb.substring(0,12000));*/
+  
+  return playlists;
+
+}
